52e5176a7ac7edb3edf774184d56de82c92afe6f
Ashwini Yadrami <yadramiash192380@gmail.com>
2024-10-17 11:11:40 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace FormatException
+{
+    class Program
+        {
+            static void Main()
+            {
+                try
+                {
+                    Console.Write("Enter a number: ");
+                    string input = Console.ReadLine();
+
+                    // Convert the input to an integer
+                    int number = int.Parse(input);
+
+                    Console.WriteLine($"The number you entered is: {number}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid number."+ex.Message);
+                }
+            }
+        }
+    }
+
